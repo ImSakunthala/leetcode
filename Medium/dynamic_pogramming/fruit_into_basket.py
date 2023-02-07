dd62147f45270604abee749c3e9f78b658549314
Sakunthala Devi Muthusamy <121899092+ImSakunthala@users.noreply.github.com>
2023-02-07 19:43:00 +0100

    Update fruit_into_basket.py
    TIL tag added

--- Medium/dynamic_pogramming/fruit_into_basket.py
+++ Medium/dynamic_pogramming/fruit_into_basket.py
@@ -48,7 +48,7 @@ class Solution:
 
         for index, tree in enumerate(fruits):
 
-            # get() -> Return the value for key if key is in the dictionary, else default. If default is not given,
+            # TIL: get() -> Return the value for key if key is in the dictionary, else default. If default is not given,
             # it defaults to None, so that this method never raises a KeyError. Here default value is Zero.
             baskets[tree] = baskets.get(tree, 0) + 1
 
