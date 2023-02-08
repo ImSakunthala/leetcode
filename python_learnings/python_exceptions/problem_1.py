bc79af0a6bcd397f0dbc7ad051a0b3ccdcdaaf83
Sakunthala Devi Muthusamy <sakunthaladevi.muthusamy@gmail.com>
2023-02-08 13:44:22 +0100

    feat: exception-handling
    solution for problem_1 in exception-handling.txt

--- python_learnings/python_exceptions/problem_1.py
+++ python_learnings/python_exceptions/problem_1.py
@@ -6,12 +6,17 @@ Handle the ZeroDivisionError exception to handle the case where the second numbe
 
 class Solution:
     def zero_division(self, numerator: int, denominator: int) -> int or str:
+        # try -> block of code in Python that is used to enclose the code that might raise an exception.
+        # The purpose of try block is to catch exceptions that are raised in the code,
+        # so that they can be handled in a more controlled way, rather than causing the program to crash.
         try:
             result = numerator // denominator
 
+        # except if the denominator is zero we can except zero division error.
         except ZeroDivisionError:
             return 'Value of denominator is Zero'
 
+        # This is final block that executed, if given denominator input is not zero
         return result
 
 
