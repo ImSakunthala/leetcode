d5a536086da1292f796c96fea27075d82234cca2
Sakunthala Devi Muthusamy <sakunthaladevi.muthusamy@gmail.com>
2023-03-31 12:07:26 +0200

    feature/leet-code/easy
    -> decrypt string from integers and '#'
    -> easy way to mend with replace but i found it difficult to approach this with simple definitions

--- Easy/String/decrypt_string_from_alphabet_to_integer.py
+++ Easy/String/decrypt_string_from_alphabet_to_integer.py
@@ -28,24 +28,26 @@ s will be a valid string such that mapping is always possible.
 """
 import string
 
+
 class Solution:
     def freqAlphabets(self, s: str) -> str:
-        separation = []
-        position = 0
-        while position+2 <= len(s)-1:
-            if s[position+2] == '#' and position + 2 <= len(s):
-                char = s[position] + s[position+1]
-                # result += chr(96 + int(s[position] + (s[position+1])))
-                separation.append(char)
-                position += 3
-        else:
-            while position < len(s):
-                separation.append(s[position])
-                position += 1
 
-        for code in
+        s = s.replace('10#', 'j').replace('11#', 'k').replace('12#', 'l').replace('13#', 'm').replace('14#',
+                                                                                                      'n').replace(
+            '15#', 'o')
+        s = s.replace('16#', 'p').replace('17#', 'q').replace('18#', 'r').replace('19#', 's').replace('20#',
+                                                                                                      't').replace(
+            '21#', 'u')
+        s = s.replace('22#', 'v').replace('23#', 'w').replace('24#', 'x').replace('25#', 'y').replace('26#',
+                                                                                                      'z')
+        s = s.replace('1', 'a').replace('2', 'b').replace('3', 'c').replace('4', 'd').replace('5', 'e').replace(
+            '6', 'f').replace('7', 'g')
+        s = s.replace('8', 'h').replace('9', 'i')
 
+        return s
 
 
 solution = Solution()
+print(solution.freqAlphabets(s="12345678910#11#12#13#14#15#16#17#18#19#20#21#22#23#24#25#26#"))
 print(solution.freqAlphabets(s="10#11#12"))
+print(solution.freqAlphabets(s = "1326#"))
